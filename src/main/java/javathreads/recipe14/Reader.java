786e9365cbc2125d532261285f0cdc5dd7e755c8
Marius Ropotica <marius.ropotica@mambu.com>
2020-12-08 05:52:08 +0200

    3 more recipes

---
+++ src/main/java/javathreads/recipe14/Reader.java
@@ -0,0 +1,33 @@
+package javathreads.recipe14;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author mropotica
+ */
+public class Reader implements Runnable {
+
+	private PriceInfo priceInfo;
+
+	public Reader(PriceInfo priceInfo) {
+
+		this.priceInfo = priceInfo;
+	}
+
+	@Override
+	public void run() {
+
+		for (int i = 0; i < 10; i++) {
+
+			System.out.printf("%s: %s: Price1: %f\n", new Date(), Thread.currentThread().getName(), priceInfo.getPrice1());
+			System.out.printf("%s: %s: Price2: %f\n", new Date(), Thread.currentThread().getName(), priceInfo.getPrice2());
+
+			try {
+				TimeUnit.SECONDS.sleep(1);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
